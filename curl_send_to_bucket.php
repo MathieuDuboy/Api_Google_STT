f2e59cf54f0c1c0d49ef6f20a43e6f835fae5588
MathieuDuboy <11943636+MathieuDuboy@users.noreply.github.com>
2019-04-12 11:34:05 +0200

    Add files via upload

--- curl_send_to_bucket.php
+++ curl_send_to_bucket.php
@@ -1,10 +1,16 @@
 <?php
 // Configuration
 define("BASE", "https://www.googleapis.com/upload/storage/v1/b/");
-$client_id     = "922634632013-cuaomxxxxxxxxxlah.apps.xxxxxxxxxx.com";
-$client_secret = "BLVKg9Bb9xxxxxxxxxxxxxYUJZOA57e";
-$refresh_token = "1/QqvTxlxxxxxxxxxRGW1mmI";
-$bucket = 'test_api_codeur'; // Nom de mon bucket sans le gs://
+$client_id     = "922634632013-cuaom4k4s17avf3u1g5tb3ksjfmlmlah.apps.googleusercontent.com";
+$client_secret = "BLVKg9Bb9nKKghxYUJZOA57e";
+$refresh_token = "1/QqvTxl0kIdJOU6dqxI7kCdgpWl-0y4MaXGW2RGW1mmI";
+
+// Fichier récupéré via l'API OVH ici
+$file = file_get_contents('bonjour-google.flac');
+
+// Unique id pour le nom du fichier
+$uniqueId    = time() . '-' . mt_rand();
+$nom_fichier = "bonjour-google-$uniqueId.flac"; // Le nom qui sera sur le bucket
 // Fin Configuration
 
 $postData = array(
@@ -14,17 +20,15 @@ $postData = array(
     "refresh_token" => $refresh_token
 );
 
-$ch       = curl_init();
+$ch = curl_init();
 curl_setopt($ch, CURLOPT_URL, "https://accounts.google.com/o/oauth2/token");
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($postData));
 $response = curl_exec($ch);
-echo $response;
 curl_close($ch);
 $r = json_decode($response, true);
 
 $accessToken = $r["access_token"];
-echo $accessToken;
 
 
 $key         = $accessToken;
@@ -33,14 +37,9 @@ $authheaders = array(
     "Content-Type: audio/flac"
 );
 
-
-$file     = file_get_contents('bonjour-google.flac');
-$fileName = 'bonjour-google.flac';
-$url      = BASE . $bucket . '/o?name=bonjour-google3.flac&uploadType=media';
-// Le paramètre &name= peut accepter un nom dynamique si vous le souhaitez.
-// Ici j'ai forcé le nom de mon fichier qui sera sur le bucket à : bonjour-google3.flac (à vous de voir ce que vous souhaitez faire)
-
-$curl = curl_init();
+$bucket = 'test_codeur2';
+$url    = BASE . $bucket . '/o?name=' . $nom_fichier . '&uploadType=media';
+$curl   = curl_init();
 
 curl_setopt($curl, CURLOPT_POSTFIELDS, $file);
 
@@ -59,7 +58,40 @@ curl_setopt($curl, CURLOPT_HEADER, true);
 
 
 $response = curl_exec($curl);
-echo $response;
 
+
+$data = array(
+    'config' => array(
+        "encoding" => 'FLAC',
+        "languageCode" => "fr-FR"
+    ),
+    "audio" => array(
+        "uri" => "gs://test_codeur2/" . $nom_fichier
+    )
+);
+
+$ch = curl_init("https://speech.googleapis.com/v1/speech:recognize?key=AIzaSyAXFJQCIO9rkl6Up0-6WrPhJEsY2AH2KlU");
+
+curl_setopt_array($ch, array(
+    CURLOPT_POST => 1,
+    CURLOPT_POSTFIELDS => json_encode($data),
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_TIMEOUT => 10,
+    CURLOPT_HTTPHEADER => array(
+        "Authorization: Bearer " . $accessToken,
+        "Content-Type: application/json"
+    )
+));
+$json = curl_exec($ch);
+curl_close($ch);
+// "results": [ { "alternatives": [ { "transcript": "Bonjour Google comment ça va", "confidence": 0.96079487 } ] } ] }
+
+// Récupèrer u  array et la valeur du transcript
+$tab = json_decode($json, true);
+
+$transcript     = $tab["results"][0]["alternatives"][0]["transcript"];
+$taux_confiance = $tab["results"][0]["alternatives"][0]["confidence"];
+
+echo "avec un taux de confiance de " . $taux_confiance . ", voici le transcript : " . $transcript;
 
 ?>
